0d8709198ccea1861aa3f543ca295d4979001327
Mohamed Abdellatif <m.latief@gmail.com>
2015-08-21 03:15:16 +0300

    feat(classify): Improve performance by using Celery multi-tasking and pre-loading of corpus

--- pyhaukka/classify.py
+++ pyhaukka/classify.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import words, stopwords
-import re
 
 from nltk.tokenize import wordpunct_tokenize
 
@@ -9,28 +8,9 @@ corpus_root = "nltk_data/corpora/"
 classifier_root = "nltk_data/classifiers"
 
 cbio_corpus = PlaintextCorpusReader(corpus_root + '/cbio_cancer_genes', fileids=".+\\.txt")
-_digits = re.compile('\d')
 
-
-def bio_chunk_features(word):
-    word_lower = word.lower()
-
-    cbio = word in cbio_corpus.words()
-    all_caps = word.isupper()
-    en_word = word_lower in words.words()
-    #stop_word = word_lower in stopwords.words()
-    has_digits = bool(_digits.search(word_lower))
-
-    # TODO: Add features such as:
-    # - Followed by words such as mutations, wild-type, alteration
-    # - Distance from such words
-    # - Frequency distribution
-
-    return {"cbio": cbio,
-            "word": en_word,
-            #"stop_word": stop_word,
-            "all_caps": all_caps,
-            "has_digits": has_digits}
+cbio_words = list(cbio_corpus.words())
+en_words = list(words.words())
 
 class BiomarkerClassifier(nltk.TaggerI):
     def __init__(self, chunked_trial):
@@ -38,24 +18,40 @@ class BiomarkerClassifier(nltk.TaggerI):
         # freq_dist = sorted(f.items(), key=lambda item: item[1])[:20]
         # {k:v for (k,v) in f.items() if v <= 10}  # choose only words with freq <= 10 least frequent
 
-        train_set = list(self.get_features_from_chunk(chunked_trial))
+        train_set = self.get_features_from_chunk(chunked_trial)
 
         self.classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+    def fast_calculate_features(self, words):
+        from pyhaukka.worker import bio_chunk_features
+        from celery import group
+        job = group(bio_chunk_features.s(w) for w in words)
+        result = job.apply_async()
+        feats = result.get()
+        return feats
 
     def get_features_from_chunk(self, chunks_tree):
         if chunks_tree.height()>3:
             raise Exception("Chunk tree is too deep to parse!")
-
+        words = []
+        labels = []
         for chunk in chunks_tree:
             if isinstance(chunk, nltk.Tree):
                 for b in chunk:
-                    yield (bio_chunk_features(b[0]), 'BIO')
+                    words.append(b[0])
+                    labels.append('BIO')
             else:
-                yield (bio_chunk_features(chunk[0]), 'O')
+                words.append(chunk[0])
+                labels.append('O')
+
+        feats = self.fast_calculate_features(words)
+        return zip(feats, labels)
 
     def tag(self, sentence):
         sent_tokenized = wordpunct_tokenize(sentence)
-        feats = [bio_chunk_features(word) for word in sent_tokenized]
+        #feats = [bio_chunk_features(word) for word in sent_tokenized]
+        #feats = [pool.apply(bio_chunk_features, args=(word,)) for word in sent_tokenized]
+        feats = self.fast_calculate_features(sent_tokenized)
         tags = self.classifier.classify_many(feats)
         conll = zip(sent_tokenized, tags)
         return conll
--- pyhaukka/worker.py
+++ pyhaukka/worker.py
@@ -1,9 +1,9 @@
 from celery import Celery
-import pyhaukka.classify
 
 celery_app = Celery(__name__)
 celery_app.config_from_object('celeryconfig')
 
+import pyhaukka.classify
 classifier = pyhaukka.classify.load_pickled()
 
 @celery_app.task(bind=True)
@@ -37,6 +37,8 @@ def process_trial(self, url):
 
         self.update_state(state='PROGRESS', meta={'status': "Tagging Biomarkers" })
         tags = classifier.tag(raw)
+
+        self.update_state(state='PROGRESS', meta={'status': "Collecting Biomarkers" })
         gner = [(t,v) for (t,v) in tags if v == 'BIO']
 
         self.update_state(state='PROGRESS', meta={'status': "Storing clinical trial" })
@@ -51,3 +53,29 @@ def process_trial(self, url):
         return {'status': 'Trial processed!', 'result': nct_id}
 
     raise urllib2.URLError(reason="URL returned no response")
+
+
+
+@celery_app.task
+def bio_chunk_features(word):
+    from pyhaukka.classify import en_words, cbio_words
+    import re
+    _digits = re.compile('\d')
+    word_lower = word.lower()
+
+    cbio = word in cbio_words
+    all_caps = word.isupper()
+    en_word = word_lower in en_words
+    #stop_word = word_lower in stopwords.words()
+    has_digits = bool(_digits.search(word_lower))
+
+    # TODO: Add features such as:
+    # - Followed by words such as mutations, wild-type, alteration
+    # - Distance from such words
+    # - Frequency distribution
+
+    return {"cbio": cbio,
+            "word": en_word,
+            #"stop_word": stop_word,
+            "all_caps": all_caps,
+            "has_digits": has_digits}
