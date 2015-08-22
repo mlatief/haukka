d891e7bc5eca6b6b41f205a64e912ce06a7875ad
Mohamed Abdellatif <m.latief@gmail.com>
2015-08-22 05:46:14 +0300

    fix(): Add pickle files to .gitattributes to treat them as binary files
    - FIX: Also fix an issue when retrieving trial with NCT_ID not stored in the DB.

--- pyhaukka/classify.py
+++ pyhaukka/classify.py
@@ -14,7 +14,7 @@ cbio_corpus = PlaintextCorpusReader(corpus_root + '/cbio_cancer_genes', fileids=
 
 cbio_words = list(cbio_corpus.words())
 en_words = list(words.words())
-stop_words = stopwords.words()
+stop_words = list(stopwords.words())
 
 punct = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(unichr(i)).startswith(u'P'))
 
@@ -97,7 +97,7 @@ def store_pickled(classifier, root="nltk_data/classifiers", fname="biomarker_cla
     import pickle
     import os
     fpath = os.path.join(root, fname)
-    with open(fpath, "wb+") as f:
+    with open(fpath, "wb") as f:
         pickle.dump(classifier, f)
 
 def load_pickled(root="nltk_data/classifiers", fname="biomarker_classifier.pickle"):
